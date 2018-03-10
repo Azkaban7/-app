748363ae285846ed58ee38a7f9886046df870184
王曦 <wangxi@wangxidemacbook-air.local>
2018-03-10 09:53:57 +0800

    Initial Commit

---
+++ 扭蛋/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  扭蛋
+//
+//  Created by 王曦 on 2018/3/10.
+//  Copyright © 2018年 曦王. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
